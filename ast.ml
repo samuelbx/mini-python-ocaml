f23301d8bd0ad3fb95c6c1c202cf7ed4d5040d55
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-02-12 16:01:43 +0100

    print ast helper methdos

--- ast.ml
+++ ast.ml
@@ -90,3 +90,62 @@ and tdef = fn * tstmt
 
 and tfile = tdef list
   (** the block of global statements is now a `main` function *)
+
+
+(* Temporary !! *)
+let rec string_of_constant = function
+  | Cnone -> "None"
+  | Cbool b -> string_of_bool b
+  | Cstring s -> "\"" ^ s ^ "\""
+  | Cint i -> Int64.to_string i
+
+let rec string_of_binop = function
+  | Badd -> "+"
+  | Bsub -> "-"
+  | Bmul -> "*"
+  | Bdiv -> "/"
+  | Bmod -> "%"
+  | Beq -> "=="
+  | Bneq -> "!="
+  | Blt -> "<"
+  | Ble -> "<="
+  | Bgt -> ">"
+  | Bge -> ">="
+  | Band -> "and"
+  | Bor -> "or"
+
+let rec string_of_unop = function
+  | Uneg -> "-"
+  | Unot -> "not"
+
+let rec string_of_ident id = id.id
+
+let rec string_of_expr = function
+  | TEcst c -> string_of_constant c
+  | TEvar v -> v.v_name
+  | TEbinop (op, e1, e2) ->
+      "(" ^ (string_of_expr e1 ^ " " ^ string_of_binop op ^ " " ^ string_of_expr e2) ^ ")"
+  | TEunop (op, e) -> string_of_unop op ^ " " ^ string_of_expr e
+  | TEcall (fn, args) ->
+      fn.fn_name ^ "(" ^ String.concat ", " (List.map string_of_expr args) ^ ")"
+  | TElist lst -> "[" ^ String.concat ", " (List.map string_of_expr lst) ^ "]"
+  | TErange e -> "range(" ^ string_of_expr e ^ ")"
+  | TEget (e1, e2) -> string_of_expr e1 ^ "[" ^ string_of_expr e2 ^ "]"
+
+let rec string_of_stmt = function
+  | TSif (cond, s1, s2) ->
+      "[if " ^ string_of_expr cond ^ ":\n" ^ string_of_stmt s1 ^ "\nelse:\n" ^ string_of_stmt s2 ^ "]"
+  | TSreturn e -> "return " ^ string_of_expr e
+  | TSassign (var, e) -> var.v_name ^ " = " ^ string_of_expr e
+  | TSprint e -> "print(" ^ string_of_expr e ^ ")"
+  | TSblock stmts -> "{" ^ String.concat "\n" (List.map string_of_stmt stmts) ^ "}"
+  | TSfor (var, e, body) ->
+      "for " ^ var.v_name ^ " in " ^ string_of_expr e ^ ":\n" ^ string_of_stmt body
+  | TSeval e -> string_of_expr e
+  | TSset (e1, e2, e3) -> string_of_expr e1 ^ "[" ^ string_of_expr e2 ^ "] = " ^ string_of_expr e3
+
+let rec string_of_tdef (fn, body) =
+  fn.fn_name ^ "(" ^ String.concat ", " (List.map (fun v -> v.v_name) fn.fn_params) ^ "):\n"
+  ^ string_of_stmt body
+
+let string_of_tfile tfile = String.concat "\n\n" (List.map string_of_tdef tfile)
