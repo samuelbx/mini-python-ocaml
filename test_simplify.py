9d93510b0e9121eb4c102d184a3657438d267cd9
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-02-12 16:47:24 +0100

    preparing what's next

---
+++ test_simplify.py
@@ -0,0 +1,44 @@
+def f(x):
+    if(True):
+        print("x")
+        if(True):
+            print("x")
+            if(True):
+                print("x")
+                if(True):
+                    print("x")
+    for i in list(range(len("samuel"))):
+        print("x"+"y"+"2")
+        print("y")
+        if i == 0+0*4291*x:
+            print(len(["Coucou", 5]))
+        else:
+            print(2<3)
+        
+        if False and True:
+            return 1
+        if False and (x == 0):
+            return 1
+        
+        if (x == 0) or False:
+            return 1
+        if (x == 42) or True:
+            return 42
+            print("ça ne devrait pas être là")
+        
+        print("ok ok ok")
+        print("ça ne devrait pas être là 2")
+        return 42
+
+        if -x <= -i + 1:
+            print("ok")
+        x = (-x)*(-x)
+        x = -x*(-x)
+        x = -2*(-x)
+        return (-2)*(-(x+2+2+4-5))
+        x = (-2)*(-(x+2+2+4-5))
+print(0)
+x = 4*4+1
+f(4*4+1)
+
+
