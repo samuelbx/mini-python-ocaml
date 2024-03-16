c10299e81543dbd4c654e0262815cff54c20a76b
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-16 18:51:08 +0100

    upd

--- test.s
+++ test.s
@@ -3,61 +3,31 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	addq $-16, %rsp
-	movq $40, %rdi
-	call malloc
-	movq %rax, -16(%rbp)
-	movq $4, %r10
-	movq $3, %r8
-	movq -16(%rbp), %r11
-	movq %r10, 0(%r11)
-	movq -16(%rbp), %r11
-	movq %r8, 8(%r11)
+	addq $-40, %rsp
 	movq $16, %rdi
 	call malloc
-	movq %rax, %r10
-	movq $2, %r9
+	movq %rax, %r9
 	movq $1, %r8
-	movq %r9, 0(%r10)
-	movq %r8, 8(%r10)
-	movq -16(%rbp), %r11
-	movq %r10, 16(%r11)
-	movq $16, %rdi
-	call malloc
-	movq %rax, %r10
-	movq $2, %r9
-	movq $2, %r8
-	movq %r9, 0(%r10)
-	movq %r8, 8(%r10)
-	movq -16(%rbp), %r11
-	movq %r10, 24(%r11)
-	movq $16, %rdi
-	call malloc
-	movq %rax, %r10
-	movq $2, %r9
-	movq $3, %r8
-	movq %r9, 0(%r10)
-	movq %r8, 8(%r10)
-	movq -16(%rbp), %r11
-	movq %r10, 32(%r11)
-	movq -16(%rbp), %r15
-	movq %r15, -8(%rbp)
-	movq $16, %rdi
-	call malloc
-	movq %rax, %r10
-	movq $2, %r9
-	movq $0, %r8
-	movq %r9, 0(%r10)
-	movq %r8, 8(%r10)
-	movq 8(%r10), %r8
+	movq $1, %r10
+	movq %r8, 0(%r9)
+	movq %r10, 8(%r9)
+	movq -8(%rbp), %r15
+	movq 0(%r15), %r8
+	movq -8(%rbp), %r15
+	movq 8(%r15), %rsi
+	movq $0, %r10
+	subq %r8, %r10
+	testq %r10, %r10
+	jz L15
+	movq $1, %r10
+	subq %r8, %r10
+	testq %r10, %r10
+	jz L21
 	movq $2, %r10
-	addq %r10, %r8
-	movq -8(%rbp), %r10
-	movq $8, %r9
-	imulq %r9, %r8
-	addq %r10, %r8
-	movq 0(%r8), %r10
-	movq 8(%r10), %r10
+	subq %r8, %r10
+	testq %r10, %r10
+	jz L23
+L11:
 	movq $16, %rdi
 	call malloc
 	movq %rax, %r10
@@ -69,4 +39,42 @@ main:
 	movq %rbp, %rsp
 	popq %rbp
 	ret
+L23:
+	movq $.LC1, %rdi
+	movq $0, %rax
+	call printf
+	movq %rax, %r10
+L13:
+	movq $10, %rdi
+	call putchar
+	movq %rax, %r10
+	jmp L11
+L21:
+	movq $0, %r10
+	subq %rsi, %r10
+	testq %r10, %r10
+	jz L17
+	movq $.LC2, %rdi
+L16:
+	movq $0, %rax
+	call printf
+	movq %rax, %r10
+	jmp L13
+L17:
+	movq $.LC3, %rdi
+	jmp L16
+L15:
+	movq $.LC4, %rdi
+	movq $0, %rax
+	call printf
+	movq %rax, %r10
+	jmp L13
 	.data
+.LC4:
+	.string "None"
+.LC2:
+	.string "True"
+.LC1:
+	.string "%d"
+.LC3:
+	.string "False"
