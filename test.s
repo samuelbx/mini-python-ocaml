5138129bbf3b2d10d38766dc4e23e268a8e317bd
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-16 17:54:19 +0100

    test

--- test.s
+++ test.s
@@ -3,32 +3,44 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	addq $-40, %rsp
+	addq $-8, %rsp
+	movq $48, %rdi
+	call malloc
+	movq %rax, -8(%rbp)
+	movq $4, %r10
+	movq $3, %r8
+	movq -8(%rbp), %r11
+	movq %r10, 0(%r11)
+	movq -8(%rbp), %r11
+	movq %r8, 8(%r11)
 	movq $24, %rdi
 	call malloc
-	movq %rax, %r9
+	movq %rax, %r10
+	movq $2, %r9
+	movq $1, %r8
+	movq %r9, 0(%r10)
+	movq %r8, 8(%r10)
+	movq -8(%rbp), %r11
+	movq %r10, 16(%r11)
+	movq $24, %rdi
+	call malloc
+	movq %rax, %r10
+	movq $2, %r9
 	movq $2, %r8
-	movq $20, %r10
-	movq %r8, 0(%r9)
-	movq %r10, 8(%r9)
-	movq %r9, -8(%rbp)
-	movq -8(%rbp), %r15
-	movq 0(%r15), %r8
-	movq -8(%rbp), %r15
-	movq 8(%r15), %rsi
-	movq $0, %r10
-	subq %r8, %r10
-	testq %r10, %r10
-	jz L15
-	movq $1, %r10
-	subq %r8, %r10
-	testq %r10, %r10
-	jz L21
-	movq $2, %r10
-	subq %r8, %r10
-	testq %r10, %r10
-	jz L23
-L11:
+	movq %r9, 0(%r10)
+	movq %r8, 8(%r10)
+	movq -8(%rbp), %r11
+	movq %r10, 24(%r11)
+	movq $24, %rdi
+	call malloc
+	movq %rax, %r10
+	movq $2, %r9
+	movq $3, %r8
+	movq %r9, 0(%r10)
+	movq %r8, 8(%r10)
+	movq -8(%rbp), %r11
+	movq %r10, 32(%r11)
+	movq -8(%rbp), %r10
 	movq $24, %rdi
 	call malloc
 	movq %rax, %r10
@@ -40,42 +52,4 @@ L11:
 	movq %rbp, %rsp
 	popq %rbp
 	ret
-L23:
-	movq $.LC1, %rdi
-	movq $0, %rax
-	call printf
-	movq %rax, %r10
-L13:
-	movq $10, %rdi
-	call putchar
-	movq %rax, %r10
-	jmp L11
-L21:
-	movq $0, %r10
-	subq %rsi, %r10
-	testq %r10, %r10
-	jz L18
-	movq $.LC2, %rdi
-L16:
-	movq $0, %rax
-	call printf
-	movq %rax, %r10
-	jmp L13
-L18:
-	movq $.LC3, %rdi
-	jmp L16
-L15:
-	movq $.LC4, %rdi
-	movq $0, %rax
-	call printf
-	movq %rax, %r10
-	jmp L13
 	.data
-.LC4:
-	.string "None"
-.LC3:
-	.string "True"
-.LC1:
-	.string "%d"
-.LC2:
-	.string "False"
