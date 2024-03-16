09ed4f595ba5f88f45f5103b739b431081700b9e
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-16 13:55:03 +0100

    all casks & leaves

--- test.s
+++ test.s
@@ -3,14 +3,26 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
+	addq $-8, %rsp
+	movq $24, %rdi
+	call malloc
+	movq %rax, -8(%rbp)
+	movq $4, %r8
+	movq $1, %r10
+	movq %r8, 0(%r15)
+	movq %r15, -8(%rbp)
+	movq %r10, 8(%r15)
+	movq %r15, -8(%rbp)
 	movq $16, %rdi
 	call malloc
-	movq %rax, %r8
-	movq $2, %r10
-	movq $88, %r9
-	movq %r10, 0(%r8)
-	movq %r9, 8(%r8)
-	movq %r8, %r10
+	movq %rax, %r10
+	movq $2, %r9
+	movq $88, %r8
+	movq %r9, 0(%r10)
+	movq %r8, 8(%r10)
+	movq %r10, 16(%r15)
+	movq %r15, -8(%rbp)
+	movq -8(%rbp), %r10
 	movq $16, %rdi
 	call malloc
 	movq %rax, %r10
