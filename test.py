a8ab658119ea2c8aa4d74591fcd44cd9ecebfb41
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-16 21:26:36 +0100

    xx

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 x = "ok"
-print(x)
+print(len(x))
--- test.s
+++ test.s
@@ -10,36 +10,36 @@ __print__:
 	movq -8(%rbp), %r15
 	movq 8(%r15), %rsi
 	cmpq $0, %r10
-	jle L32
+	jle L33
 	cmpq $1, %r10
-	jle L36
+	jle L37
 	cmpq $2, %r10
-	jle L38
+	jle L39
 	cmpq $3, %r10
-	jle L54
+	jle L55
 	cmpq $4, %r10
-	jle L70
-L28:
+	jle L71
+L29:
 	movq -16(%rbp), %rax
 	movq %rbp, %rsp
 	popq %rbp
 	ret
-L70:
+L71:
 	movq $0, -40(%rbp)
-L69:
+L70:
 	movq -8(%rbp), %r15
 	movq 8(%r15), %r10
 	movq $1, -48(%rbp)
 	movq $2, %r8
 	subq -40(%rbp), %r10
 	testq %r10, %r10
-	jnz L64
-L30:
+	jnz L65
+L31:
 	movq $10, %rdi
 	call putchar
 	movq %rax, %r10
-	jmp L28
-L64:
+	jmp L29
+L65:
 	movq -40(%rbp), %r10
 	addq %r8, %r10
 	movq $8, %r8
@@ -51,17 +51,17 @@ L64:
 	movq -40(%rbp), %r15
 	addq -48(%rbp), %r15
 	movq %r15, -40(%rbp)
-	jmp L69
-L54:
+	jmp L70
+L55:
 	movq $0, -24(%rbp)
-L53:
+L54:
 	movq -8(%rbp), %r15
 	movq 8(%r15), %r10
 	movq $1, -32(%rbp)
 	movq $2, %r8
 	subq -24(%rbp), %r10
 	testq %r10, %r10
-	jz L30
+	jz L31
 	movq -24(%rbp), %r10
 	addq %r8, %r10
 	movq $8, %r8
@@ -73,31 +73,31 @@ L53:
 	movq -24(%rbp), %r15
 	addq -32(%rbp), %r15
 	movq %r15, -24(%rbp)
-	jmp L53
-L38:
+	jmp L54
+L39:
 	movq $.LC1, %rdi
 	movq $0, %rax
 	call printf
 	movq %rax, %r10
-	jmp L30
-L36:
+	jmp L31
+L37:
 	cmpq $0, %rsi
-	jle L34
+	jle L35
 	movq $.LC2, %rdi
-L33:
+L34:
 	movq $0, %rax
 	call printf
 	movq %rax, %r10
-	jmp L30
-L34:
+	jmp L31
+L35:
 	movq $.LC3, %rdi
-	jmp L33
-L32:
+	jmp L34
+L33:
 	movq $.LC4, %rdi
 	movq $0, %rax
 	call printf
 	movq %rax, %r10
-	jmp L30
+	jmp L31
 __len__:
 	pushq %rbp
 	movq %rsp, %rbp
@@ -122,6 +122,8 @@ main:
 	movq %r10, 16(%rdi)
 	movq $107, %r10
 	movq %r10, 24(%rdi)
+	call __len__
+	movq %rax, %rdi
 	call __print__
 	movq $16, %rdi
 	call malloc
