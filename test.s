0940232d977330ea0f5c072fb87aa8500ea3451b
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-16 13:51:37 +0100

    x

--- test.s
+++ test.s
@@ -3,16 +3,16 @@
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	addq $-16, %rsp
+	addq $-8, %rsp
 	movq $24, %rdi
 	call malloc
-	movq %rax, -16(%rbp)
-	movq $4, %r10
-	movq $1, %r8
-	movq %r10, 0(%r15)
-	movq %r15, -16(%rbp)
-	movq %r8, 8(%r15)
-	movq %r15, -16(%rbp)
+	movq %rax, -8(%rbp)
+	movq $4, %r8
+	movq $1, %r10
+	movq %r8, 0(%r15)
+	movq %r15, -8(%rbp)
+	movq %r10, 8(%r15)
+	movq %r15, -8(%rbp)
 	movq $16, %rdi
 	call malloc
 	movq %rax, %r10
@@ -20,29 +20,8 @@ main:
 	movq %r8, 0(%r10)
 	movq %r8, 8(%r10)
 	movq %r10, 16(%r15)
-	movq %r15, -16(%rbp)
-	movq -16(%rbp), %r10
-	movq $16, %rdi
-	call malloc
-	movq %rax, %r10
-	movq $2, %r9
-	movq $0, %r8
-	movq %r9, 0(%r10)
-	movq %r8, 8(%r10)
-	movq 8(%r10), %r15
 	movq %r15, -8(%rbp)
-	movq $16, %rdi
-	call malloc
-	movq %rax, %r9
-	movq $2, %r8
-	movq $0, %r10
-	movq %r8, 0(%r9)
-	movq %r10, 8(%r9)
-	movq $8, %r8
 	movq -8(%rbp), %r10
-	imulq %r8, %r10
-	addq %r9, %r10
-	movq 0(%r10), %r10
 	movq $16, %rdi
 	call malloc
 	movq %rax, %r10
