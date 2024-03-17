19e7507b08b1ffcada6f40291a02f7f90bb91cb0
Samuel Boïté <samuel.boite@polytechnique.edu>
2024-03-17 01:08:32 +0100

    Revert "test"
    This reverts commit 96df2b9f818d0182edaa1351f9d7c89e07c7cd21.

--- ertltree.ml
+++ ertltree.ml
@@ -50,3 +50,75 @@ let def_use = function
   | Ecall (_, n, _) -> (Register.caller_saved, prefix n Register.parameters)
   | Egoto _ | Ealloc_frame _ | Edelete_frame _ -> ([], [])
   | Ereturn -> ([], Register.rax :: Register.callee_saved)
+
+(** Temporary debugging *)
+
+open Format
+open Pp
+
+let print_instr fmt = function
+  | Econst (Cint n, r, l) -> fprintf fmt "mov $%Ld %a;  goto %a" n Register.print r Label.print l
+  | Econst (Cstring n, r, l) -> fprintf fmt "mov some_string %a;  goto %a" Register.print r Label.print l
+  | Eload (r1, n, r2, l) -> fprintf fmt "mov %d(%a) %a;  goto %a" n Register.print r1 Register.print r2 Label.print l
+  | Estore (r1, r2, n, l) -> fprintf fmt "mov %a %d(%a);  goto %a" Register.print r1 n Register.print r2 Label.print l
+  | Emunop (op, r1, l) -> fprintf fmt "%a %a;  goto %a" print_munop op Register.print r1 Label.print l
+  | Embinop (Mmov, r1, r2, l) -> fprintf fmt "mov %a %a;  goto %a" Register.print r1 Register.print r2 Label.print l
+  | Embinop (op, r1, r2, l) ->
+      fprintf fmt "%a %a %a;  goto %a" print_mbinop op Register.print r1 Register.print r2 Label.print l
+  | Emubranch (op, r, l1, l2) ->
+      fprintf fmt "%a %a;  goto %a, %a" print_mubranch op Register.print r Label.print l1 Label.print l2
+  | Embbranch (op, r1, r2, l1, l2) ->
+      fprintf fmt "%a %a %a;  goto %a, %a" print_mbbranch op Register.print r1 Register.print r2 Label.print l1
+        Label.print l2
+  | Ecall (x, n, l) -> fprintf fmt "call %s(%d);  goto %a" x n Label.print l
+  | Egoto l -> fprintf fmt "goto %a" Label.print l
+  | Ealloc_frame l -> fprintf fmt "alloc_frame;  goto %a" Label.print l
+  | Edelete_frame l -> fprintf fmt "delete_frame;  goto %a" Label.print l
+  | Eget_param (n, r, l) -> fprintf fmt "mov stackp(%d) %a --> %a" n Register.print r Label.print l
+  | Epush_param (r, l) -> fprintf fmt "push %a;  goto %a" Register.print r Label.print l
+  | Ereturn -> fprintf fmt "return"
+
+let succ = function
+  | Econst (_, _, l)
+  | Eload (_, _, _, l)
+  | Estore (_, _, _, l)
+  | Emunop (_, _, l)
+  | Embinop (_, _, _, l)
+  | Ecall (_, _, l)
+  | Egoto l
+  | Ealloc_frame l
+  | Edelete_frame l
+  | Eget_param (_, _, l)
+  | Epush_param (_, l) ->
+      [ l ]
+  | Emubranch (_, _, l1, l2) | Embbranch (_, _, _, l1, l2) -> [ l1; l2 ]
+  | Ereturn -> []
+
+let visit f g entry liveness =
+  let visited = Hashtbl.create 97 in
+  let rec visit l =
+    if not (Hashtbl.mem visited l) then (
+      Hashtbl.add visited l ();
+      let i = Label.M.find l g in
+      let liveinfo = Label.M.find l liveness in
+      f l i liveinfo;
+      List.iter visit (succ i))
+  in
+  visit entry
+
+let print_set = Register.print_set
+
+let print_live_info fmt li =
+  fprintf fmt "d={%a}@ u={%a}@ i={%a}@ o={%a}" print_set li.defs print_set li.uses print_set li.ins print_set li.outs
+
+let print_graph fmt = visit (fun l i liveinfo -> fprintf fmt "%a: %a @\n" Label.print l print_instr i)
+
+let print_ertldef fmt liveness f =
+  fprintf fmt "%s(%d)@\n" f.fun_name f.fun_formals;
+  fprintf fmt "  @[";
+  fprintf fmt "entry : %a@\n" Label.print f.fun_entry;
+  fprintf fmt "locals: @[%a@]@\n" Register.print_set f.fun_ctx;
+  print_graph fmt f.fun_body f.fun_entry liveness;
+  fprintf fmt "@]@."
+
+let print_ertlfile fmt p = List.iter (print_ertldef fmt p.liveness) p.funs
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-def f(i, j):
-    if i >= j:
-        return []
-    return [i] + f(i+1, j)
+def fact(n):
+  if n == 0:
+    return 1
+  else:
+    return n*fact(n-1)
 
-print([] + [])
-print([1,2] + [3,4,5])
-print(f(4, 7))
+print(fact(5))
